dfbb90569ca6bb1dfdb2ac442305b9ec129a7ab4
ShinHyunJin <jake@mindbloom.co.kr>
2024-07-29 16:32:16 +0900

    spring boot 합체

---
+++ src/test/java/com/standard/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.standard.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
